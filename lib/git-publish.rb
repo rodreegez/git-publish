aac440d3d5e5ce74f82e7d75f0a7fd84afb8a8b6
Adam <electronicbattleweapon7@gmail.com>
2011-06-18 15:09:44 +0100

    actually publish git branches

--- lib/git-publish.rb
+++ lib/git-publish.rb
@@ -4,7 +4,20 @@ class GitPublish
   def run
     git_dir = `git rev-parse --git-dir`
     repo = Grit::Repo.new(File.dirname(git_dir))
-    current_branch_name = repo.head.name
-    p current_branch_name
+    @local_branch_name = repo.head.name
+    origin(ARGV[0])
+    remote_branch_name(ARGV[1])
+    %x|git push #{origin} #{@local_branch_name}:refs/heads/#{remote_branch_name}|
+    %x|git fetch #{origin}|
+    %x|git branch --track #{@local_branch_name} #{origin}/#{remote_branch_name}|
+    %x|git checkout #{@local_branch_name}|
+  end
+
+  def origin(origin=nil)
+    @origin ||= "origin"
+  end
+
+  def remote_branch_name(remote_branch_name=nil)
+    @remote_branch_name ||= @local_branch_name
   end
 end
