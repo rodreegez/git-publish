d5995b860e51ce3b59d9a6a25a7b62e557a0f091
Adam <electronicbattleweapon7@gmail.com>
2011-06-18 13:26:53 +0100

    print current branch name

--- lib/git-publish.rb
+++ lib/git-publish.rb
@@ -1,5 +1,10 @@
+require 'grit'
+
 class GitPublish
   def run
-    p "BACON"
+    git_dir = `git rev-parse --git-dir`
+    repo = Grit::Repo.new(File.dirname(git_dir))
+    current_branch_name = repo.head.name
+    p current_branch_name
   end
 end
