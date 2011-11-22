aa95e5c68eae92e38ccfdbb8777461fce1298c9e
Adam <electronicbattleweapon7@gmail.com>
2011-11-22 13:55:36 +0000

    push and track in one go, add warnings for important branches

--- lib/git-publish.rb
+++ lib/git-publish.rb
@@ -4,7 +4,6 @@ class GitPublish
   include Helpers
   def run
     init(ARGV[0], ARGV[1])
-    %x|git push #{@origin} #{@local_branch_name}:refs/heads/#{@remote_branch_name}|
-    %x|git branch --set-upstream #{@local_branch_name} #{@origin}/#{@remote_branch_name}|
+    %x|git push -u #{@origin} #{@local_branch_name}:refs/heads/#{@remote_branch_name}|
   end
 end
--- lib/git-unpublish.rb
+++ lib/git-unpublish.rb
@@ -4,8 +4,18 @@ class GitUnpublish
   include Helpers
   def run
     init(ARGV[0], ARGV[1])
-    %x|git push #{@origin} :#{@remote_branch_name}|
-    puts %Q|\n  Run \`git branch -d #{@local_branch_name}\` to delete the branch locally\n |
+    DANGER = %w[test development master dev develop staging production]
+    if DANGER.include? @remote_branch_name
+      puts "It looks like you are trying to delete an important branch. Are you sure you want to continue?"
+      puts "type \"delete #{@remote_branch_name}\" (without quotes) to continue."
+      continue = gets.strip
+      if contunue != "delete #{@remote_branch_name}"
+        puts "aborting"
+      else
+        %x|git push #{@origin} :#{@remote_branch_name}|
+        puts %Q|\n  Run \`git branch -d #{@local_branch_name}\` to delete the branch locally\n |
+      end
+    end
   end
 end
 
